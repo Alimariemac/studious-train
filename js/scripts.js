3c7ff18b26ac4571c78042d7a38690eb425d77c5
Alicia MacCara <Aliciamaccara@gmail.com>
2020-03-02 11:05:01 -0500

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+  (function() {
+
+        var item = document.querySelector('#scroll-section');
+        window.addEventListener('wheel', function(e){
+            if (e.deltaY > 0) {
+                item.scrollLeft += 100;
+            }
+
+            else {
+                item.scrollLeft -= 100;
+            }
+        });
+    })();
