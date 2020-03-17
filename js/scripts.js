156733a17638a686836d32e127c89a48d59787f7
Alicia MacCara <Aliciamaccara@gmail.com>
2020-03-17 19:10:16 -0400

    added jquery

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,30 @@
             $("#my-modal").modal("show");
            
         });
+
+        function animateCSS(element, animationName, callback) {
+            const node = document.querySelector(element)
+            node.classList.add('animated', animationName)
+        
+            function handleAnimationEnd() {
+                node.classList.remove('animated', animationName)
+                node.removeEventListener('animationend', handleAnimationEnd)
+        
+                if (typeof callback === 'function') callback()
+            }
+        
+            node.addEventListener('animationend', handleAnimationEnd)
+        }
+
+        let win = $(window)
+
+        win.scroll(function(event){
+            image.each(function(i,el){
+                var el = $(el);
+                if($(this).visible(true)){
+                    $(this).addClass("animated slideInUp fadeIn")
+                }
+            })
+        })
+        
     })();
